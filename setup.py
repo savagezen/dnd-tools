28ee6d7dd3e98905e9595813eebd8be93e96d5dd
German M. Bravo <german.mb@deipi.com>
2017-11-01 17:27:59 -0600

    Improvements
    + PEP 8
    + Lots of missing variables/misspellings
    + Using random with system's PRNG if possible
    + Python 2 support
    + Package configuration for setup.py
    + Normalized descriptions

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 from distutils.core import setup
 
 setup(
-  scripts = [
-    'scripts/dnd-tools'
-  ]
+    name='dnd-tools',
+    version='0.1.0-beta.1',
+    license='AGPL',
+    scripts=[
+        'scripts/dnd-tools'
+    ]
 )
